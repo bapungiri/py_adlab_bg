c81e902dbd493c621c6463612dc0daa50128a869
bapungiri <bapungiri.codespace@outlook.com>
2025-06-13 18:34:27 +0530

    new colors

--- mab_colors.py
+++ mab_colors.py
@@ -11,3 +11,14 @@ def colors_2arm(amount=1):
         # adjust_lightness("#f72585", amount=amount),  # Unstruc
         adjust_lightness("#3baaa1", amount=amount),  # Struc
     ]
+
+
+def colors_2arm_swap(amount=1):
+    return [
+        adjust_lightness("#f55673", amount=amount),  # Unstruc
+        adjust_lightness("#ea8c67", amount=amount),  # Unstruc on struc
+        adjust_lightness("#3baaa1", amount=amount),  # Struc
+        adjust_lightness("#4d89cd", amount=amount),  # Struc on unstruc
+    ]
+
+
