2f9e3bcae662426ce47c7252a4d41a0571698462
bapungiri <bapungiri.codespace@outlook.com>
2025-12-07 16:59:45 +0530

    color changes for aarushi's poster

--- mab_colors.py
+++ mab_colors.py
@@ -7,9 +7,11 @@ def colors_2arm(amount=1):
     #     adjust_lightness("#3980ea", amount=amount),  # Struc
     # ]
     return [
-        adjust_lightness("#f55673", amount=amount),  # Unstruc
+        # adjust_lightness("#f55673", amount=amount),  # Unstruc
         # adjust_lightness("#f72585", amount=amount),  # Unstruc
-        adjust_lightness("#3baaa1", amount=amount),  # Struc
+        # adjust_lightness("#3baaa1", amount=amount),  # Struc
+        adjust_lightness("#Ff7518", amount=amount),  # Unstruc
+        adjust_lightness("#008000", amount=amount),  # Struc
     ]
 
 
@@ -20,5 +22,3 @@ def colors_2arm_swap(amount=1):
         adjust_lightness("#3baaa1", amount=amount),  # Struc
         adjust_lightness("#4d89cd", amount=amount),  # Struc on unstruc
     ]
-
-
