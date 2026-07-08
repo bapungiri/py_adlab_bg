24eeb26407a624bc5aa4f797e2a19855f489aa15
bapungiri <bapungiri.codespace@outlook.com>
2026-07-08 10:45:09 +0530

    various edits

--- mab_subjects.pyi
+++ mab_subjects.pyi
@@ -21,6 +21,7 @@ class GroupData:
     fit_multi_policy_lesion: VersionedAccessor
     fit_qlearnH: VersionedAccessor
     fit_qlearnH_sim: VersionedAccessor
+    fit_qlearn_combinations_lesion: VersionedAccessor
     fit_qlearn_corr_uncorr: VersionedAccessor
     fit_qlearn_easy_hard: VersionedAccessor
     fit_qlearn_low_high_combinations: VersionedAccessor
